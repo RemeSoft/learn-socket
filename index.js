f1fc3248f15c6bc7f5aeec7c0f74e443e35344c9
remesoft <remesoft.development@gmail.com>
2023-08-23 00:03:39 +0600

    Class 6

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const http= require('http');
 const expressServer = http.createServer(app);
 
+const {Server} = require('socket.io');
+const io = new Server(expressServer);
+
 app.get('/',function (req, res){
     res.sendFile(__dirname+"/index.html")
 });
